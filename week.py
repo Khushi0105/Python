39da165baf204ac0d3e1c716bddc46aa09690874
unknown <you@example.com>
2025-06-25 10:45:16 +0530

    week.py

---
+++ week.py
@@ -0,0 +1,17 @@
+week = int(input("enter the week no"))
+if(week ==1):
+    print ("the day is monday")
+elif( week==2):
+    print("the day is tuesday")
+elif(week==3):
+    print("the day is wednesday")
+elif(week==4):
+    print("the day is thrusday")
+elif(week==5):
+    print("the day is friday")
+elif(week==6):
+    print("the day is saturday")
+elif(week==7):
+    print("the day is sunday")
+else:
+    print ("not define")
